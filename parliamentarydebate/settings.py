4fd6c71b4c083b5f592e1fdd6e8a6d9dd2cc8225
unknown <ankit11hab@gmail.com>
2022-11-21 19:06:42 +0530

    req

--- parliamentarydebate/settings.py
+++ parliamentarydebate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'r1%0f3pdj+8bs#!m+#dbb(++z7ix%2)zr_774bb6^u)s&bfoyp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
